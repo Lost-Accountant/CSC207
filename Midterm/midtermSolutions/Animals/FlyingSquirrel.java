cf7869fd9513bf36d3912ac7d284f7f354f6b013
Lost-Accountant <gxsgtghost141@gmail.com>
2020-11-10 22:31:16 -0500

    finished midterm

---
+++ Midterm/midtermSolutions/Animals/FlyingSquirrel.java
@@ -0,0 +1,11 @@
+package ca.utoronto.utm.animal;
+
+public class FlyingSquirrel extends LandAnimal implements CanFly {
+	public FlyingSquirrel(String name, int age){
+		super(name,age);
+	}
+	@Override
+	public void fly() {
+		System.out.println(this.getName()+" is flying");
+	}
+}
---
+++ Midterm/midtermSolutions/Animals/LandAnimal.java
@@ -0,0 +1,13 @@
+package ca.utoronto.utm.animal;
+
+public class LandAnimal extends Animal {
+	public LandAnimal(String name, int age){
+		super(name,age);
+	}
+	public void walk(){
+		System.out.println(this.getName()+" is walking");
+	}
+	public String toString(){
+		return super.toString()+", and a land animal";
+	}
+}
