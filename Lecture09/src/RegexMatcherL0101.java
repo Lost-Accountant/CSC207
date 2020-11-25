6c03773f5c796459cc902f052ef091b541f80a1f
Lost-Accountant <gxsgtghost141@gmail.com>
2020-11-24 21:22:01 -0500

    Lecture 10 - IO demo

--- Lecture09/src/RegexMatcherL0101.java
+++
@@ -1,6 +0,0 @@
-public class RegexMatcherL0101 {
-    // come up with a string that satisfies
-    // ^[abc]*$ but not a*b*c*$
-
-
-}
---
+++ Lecture10/src/ioTest.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+public class ioTest {
+
+    public static void main(String [] args){
+        FileInputStream fis = new FileInputStream("filename");
+        // .read() - reads 1 byte at a time
+
+        FileReader fr = new FileReader("filename");
+        // .read()  - reads 2 byte / 1 char at a time
+        // .readline() - reads the whole line
+
+        BufferedReader br = new BufferedReader(fr);
+        // good practice
+        // .readline() - read teh whole line from console.
+    }
+}
