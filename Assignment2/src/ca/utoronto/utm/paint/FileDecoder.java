e7d9301f8ad3582b3c3aec60c718dae340c9aa1b
Lost-Accountant <gxsgtghost141@gmail.com>
2021-01-11 22:56:50 -0500

    Finished loading lines.
    TODO: recognizeShape and recognizePoint.

--- Assignment2/src/ca/utoronto/utm/paint/FileDecoder.java
+++ Assignment2/src/ca/utoronto/utm/paint/FileDecoder.java
@@ -5,7 +5,10 @@ import ca.utoronto.utm.paint.Command.AddPointCommand;
 import ca.utoronto.utm.paint.Command.AddShapeCommand;
 import ca.utoronto.utm.paint.Command.Command;
 import ca.utoronto.utm.paint.Configuration.Configuration;
+import ca.utoronto.utm.paint.Line.Line;
 import ca.utoronto.utm.paint.Line.LineComponent;
+import ca.utoronto.utm.paint.Line.PolyLine;
+import ca.utoronto.utm.paint.Line.Squiggle;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -29,29 +32,31 @@ public class FileDecoder {
         if(commandMatcher.matches()){
             // step 1: find out command type
             String commandType = line.substring(0, line.indexOf(","));
+            //System.out.println(commandType);
 
             // step 2: find out configuration
             Configuration storedConfig = recognizeConfiguration(line);
-
-            System.out.println(recognizePoints(line));
+            //System.out.println(storedConfig);
 
             // step 3: find out about the object
             Object creation = recognizeObject(line, commandType);
+            //System.out.println(creation);
 
             // step 4: construct and deliver the command
             constructedCommand = constructCommand(commandType, creation, model);
+            System.out.println(constructedCommand);
+            return constructedCommand;
         }
         return null;
     }
 
     public Configuration recognizeConfiguration(String line){
         Pattern configPattern = Pattern.compile("configuration=\\(java\\.awt\\.Color\\[" +
-                "r=(0*(?:[0-9][0-9]?|[0-2][0-5][0-5]))\\,g=(0*(?:[0-9][0-9]?|[0-2][0-5][0-5]))," +
-                "b=(0*(?:[0-9][0-9]?|[0-2][0-5][0-5]))]\\,\\s(10|[1-9])\\, (true|false)\\)");
+                "r=(\\d{1,3})\\,g=(\\d{1,3}),b=(\\d{1,3})]\\,\\s(10|[1-9])\\, (true|false)\\)");
         // Find section
         Matcher configMatcher = configPattern.matcher(line);
         if(configMatcher.find()){
-            System.out.println(configMatcher.group(0));
+            //System.out.println(configMatcher.group(0));
 
             Color storedColor = new Color(Integer.parseInt(configMatcher.group(1)),
                     Integer.parseInt(configMatcher.group(2)),
@@ -69,11 +74,11 @@ public class FileDecoder {
 
     public Object recognizeObject(String line, String commandType){
         // judge based on rough type
-        if (commandType == "Shape"){
+        if (commandType.equals("Shape")){
             return recognizeShape(line);
-        } else if (commandType == "Line"){
+        } else if (commandType.equals("Line")){
             return recognizeLine(line);
-        } else if (commandType == "Point"){
+        } else if (commandType.equals("Point")){
             return recognizePoint(line);
         } else {
             return null;
@@ -81,9 +86,40 @@ public class FileDecoder {
     }
 
     public LineComponent recognizeLine(String line){
-        // judge specific line
-        ArrayList<Point> pointsSaved = recognizePoints(line);
 
+        // judge specific line
+        Matcher lineMatcher = Pattern.compile(",\\s([a-zA-z]*)\\{").matcher(line);
+        if (lineMatcher.find()){
+            ArrayList<Point> pointsSaved = recognizePoints(line);
+
+            String lineType = lineMatcher.group(1);
+            if (lineType.equals("Line")){
+                Line storedLine = new Line(pointsSaved.get(0), pointsSaved.get(1), recognizeConfiguration(line));
+                return storedLine;
+            } else if (lineType.equals("Squiggle")){
+                Squiggle storedLine = new Squiggle(pointsSaved.get(0), recognizeConfiguration(line));
+
+                int i = 1;
+                while(i < pointsSaved.size()){
+                    storedLine.addPoint(pointsSaved.get(i));
+                    i += 1;
+                }
+                return storedLine;
+
+            } else if (lineType.equals("PolyLine")){
+                PolyLine storedLine = new PolyLine(pointsSaved.get(0), recognizeConfiguration(line));
+
+                int i = 1;
+                while(i < pointsSaved.size()){
+                    storedLine.addPoint(pointsSaved.get(i));
+                    i += 1;
+                }
+
+                return storedLine;
+            } else {
+                return null;
+            }
+        }
         return null;
     }
 
@@ -119,15 +155,15 @@ public class FileDecoder {
             // add each point to collection
             collectedPoints.add(eachPoint);
         }
-        return null;
+        return collectedPoints;
     }
 
     private Command constructCommand(String commandType, Object creation, PaintModel model){
-        if (commandType == "Point" && creation instanceof Point){
+        if (commandType.equals("Point") && creation instanceof Point){
             constructedCommand = new AddPointCommand(model, (Point) creation);
-        } else if (commandType == "Line" && creation instanceof LineComponent){
+        } else if (commandType.equals("Line") && creation instanceof LineComponent){
             constructedCommand = new AddLineCommand(model, (LineComponent) creation);
-        } else if (commandType == "Shape" && creation instanceof Shape){
+        } else if (commandType.equals("Shape") && creation instanceof Shape){
             constructedCommand = new AddShapeCommand(model, (ca.utoronto.utm.paint.Shape.Shape) creation);
         }
         return constructedCommand;
