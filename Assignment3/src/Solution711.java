f7f719eda3186dd3720a59271e2b3905df8c0598
Lost-Accountant <gxsgtghost141@gmail.com>
2021-01-17 21:58:46 -0500

    Assignment Q2 finished. Recursive version finished too.

--- Assignment3/src/Solution711.java
+++ Assignment3/src/Solution711.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution711 {
 
     public static String solve(double price){
@@ -32,9 +35,51 @@ public class Solution711 {
         return "";
     }
 
+    public static ArrayList<Integer> solveRecursive(double price, int number, ArrayList<Integer> previous, double originalP){
+        if (price == originalP){
+            previous = new ArrayList<Integer>();
+        }
+
+        if (number == 1){
+            int x = (int) Math.round(price * 100);
+            int product = x;
+            for (int each: previous){
+                product = product * each;
+            }
+
+            int finalPrice = (int) Math.round(originalP * Math.pow(100, previous.size() + 1));
+            if (product == finalPrice){
+                previous.add(x);
+                System.out.println("a = " + (double) previous.get(0)/100 + ", " +
+                        "b = " + (double) previous.get(1)/100 + ", " +
+                        "c = " + (double) previous.get(2)/100 + ", " +
+                        "d = " + (double) previous.get(3)/100);
+                return previous;
+            } else {
+
+            }
+        } else {
+            int transformed = (int) Math.round(price * 100);
+            for (int x = 1; x < transformed; x++){
+                int maxY = transformed -  x;
+                previous.add(x);
+                //System.out.println(previous);
+                ArrayList<Integer> result = solveRecursive((double) maxY/100, number - 1, previous, originalP);
+                if (result != null){
+                    return null;
+                }
+                previous.remove(previous.size() - 1);
+                //System.out.println(previous);
+                // if that was a dead end.
+            }
+        }
+        return null;
+    }
+
 
     public static void main(String[] args){
         System.out.println(solve(7.11)); // solves within 1 sec, probably because of 1 less loop.
-        // System.out.println(solve(7.11)); // very slow
+        System.out.println(solveRecursive(7.11, 4, null, 7.11));
+
     }
 }
